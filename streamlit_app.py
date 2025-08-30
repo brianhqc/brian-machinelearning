b7b6c8a0695e3cd91fa06f1ef7f13dd2ff7a8782
brianhqc <121365978+brianhqc@users.noreply.github.com>
2025-08-29 23:38:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title('🎈 Machine Learning App')
+st.title('Now it's working')
 
 df = pd.read_csv('https://raw.githubusercontent.com/brianhqc/brian-machinelearning/refs/heads/master/Telco-Customer-Churn.csv')
 df
