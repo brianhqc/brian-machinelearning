91b78c3c9f1ef3f1e1c2d999436763ecab6e6fbd
brianhqc <121365978+brianhqc@users.noreply.github.com>
2025-08-29 23:40:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,7 @@ import pandas as pd
 
 st.title('The Telcom Churn Prediction App')
 
-df = pd.read_csv('https://raw.githubusercontent.com/brianhqc/brian-machinelearning/refs/heads/master/Telco-Customer-Churn.csv')
-df
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/brianhqc/brian-machinelearning/refs/heads/master/Telco-Customer-Churn.csv')
+  df
